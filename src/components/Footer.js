0b4b19c27d48dad6776be5f446c8ad3c9975730a
theoklitosBam7 <th.bampouris@gmail.com>
2019-01-06 01:05:15 +0200

    Added bampouris.eu link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,6 +11,36 @@ class Footer extends React.Component {
           paddingTop: rhythm(1),
         }}
       >
+        <div style={{ float: 'left' }}>
+          <a
+            href="https://www.linkedin.com/in/theoklitosbam7"
+            target="_blank"
+            rel="noopener noreferrer">
+            Linkedin
+          </a> &bull;{' '}
+          <a
+            href="https://twitter.com/theoklitosBam7"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Twitter
+          </a> &bull;{' '}
+          <a
+            href="https://github.com/theoklitosBam7"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github
+          </a> &bull;{' '}
+          
+          <a
+            href="https://www.bampouris.eu"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            bampouris.eu
+          </a>
+        </div>
         <div style={{ float: 'right' }}>
           <a
             href="https://github.com/gatsbyjs/gatsby-starter-blog"
@@ -18,26 +48,6 @@ class Footer extends React.Component {
             rel="noopener noreferrer"
           >Based on gatsby-starter-blog</a>
         </div>
-        <a
-          href="https://twitter.com/theoklitosBam7"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Twitter
-        </a> &bull;{' '}
-        <a
-          href="https://github.com/theoklitosBam7"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Github
-        </a> &bull;{' '}
-        <a
-          href="https://www.linkedin.com/in/theoklitosbam7"
-          target="_blank"
-          rel="noopener noreferrer">
-          Linkedin
-        </a>
       </footer>
     )
   }
